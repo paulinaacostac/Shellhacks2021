fd1877af86ac6f067be9338c19d6548b422dcb3e
Francisco <63214442+frankymz@users.noreply.github.com>
2021-09-26 10:54:50 -0400

    fixed bug

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -1,54 +1,52 @@
-import React from 'react';
+import React from "react";
 import { AiFillHome } from "react-icons/ai";
 import { FiUser } from "react-icons/fi";
 import { Link } from "react-router-dom";
 
-export default function Footer(){
-
-
-    return (
-
-        <React.Fragment>
-
-            <div>
-            <div
-        style={{
-          position: "absolute",
-          left: 0,
-          right: 0,
-          bottom: 0,
-          background: "#F3F3F3",
-          width: "100%",
-          height: "40px",
-          textAlign: "center",
-        }}
-      >
+export default function Footer() {
+  return (
+    <React.Fragment>
+      <div>
         <div
           style={{
-            justifyContent: "space-between",
-            maxWidth: "90px",
-            display: "flex",
-            margin: "auto",
-            alignItems: "center",
+            position: "absolute",
+            left: 0,
+            right: 0,
+            bottom: 0,
+            background: "#F3F3F3",
+            width: "100%",
+            height: "40px",
+            textAlign: "center",
           }}
         >
-          <Link
+          <div
             style={{
-              textDecoration: "none",
-              color: "black",
-              fontSize: "25px",
-              paddingTop: "5px",
+              justifyContent: "space-between",
+              maxWidth: "90px",
+              display: "flex",
+              margin: "auto",
+              alignItems: "center",
             }}
           >
-            <AiFillHome />{" "}
-          </Link>
-          <Link to="/Profile" style={{ color: "black", fontSize: "25px", paddingTop: "5px" }}>
-            <FiUser />{" "}
-          </Link>{" "}
+            <Link
+              style={{
+                textDecoration: "none",
+                color: "black",
+                fontSize: "25px",
+                paddingTop: "5px",
+              }} to="/Main"
+            >
+              <AiFillHome />{" "}
+            </Link>
+            <Link
+              to="/Profile"
+              style={{ color: "black", fontSize: "25px", paddingTop: "5px" }}
+            >
+              <FiUser />{" "}
+            </Link>{" "}
+          </div>
         </div>
       </div>
-
-            </div>
-        </React.Fragment>
-    )
+    </React.Fragment>
+  );
 }
