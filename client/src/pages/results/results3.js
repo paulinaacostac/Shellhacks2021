162caaa6e7cb46167dbaa37a40213705529cab3b
Francisco <63214442+frankymz@users.noreply.github.com>
2021-09-26 10:35:29 -0400

    listed card objects

--- client/src/pages/results/results3.js
+++ client/src/pages/results/results3.js
@@ -3,25 +3,64 @@ import { Link } from "react-router-dom";
 import Card from "../../components/card/card";
 import "../home/home.css";
 
-
-export default function Results3(){
-
-    return (
-      <React.Fragment>
-        
-        <Link to="/Main" style={{ textDecoration: "none", margin: "5px" }}>
-          {" "}
-          &#10094;{" "}
-        </Link>
-        <div className="font" style={{textAlign:"center", fontWeight: "600", padding:"20px"}}>Detect objects around me</div>
-        <div className="font" style={{ fontSize: "20px", fontWeight: "600" }}>
-          <div style={{marginLeft:"16px", marginBottom:"11px"}}>There are: </div>
-          <Card text="4" />
-          <div style={{marginLeft:"16px", marginTop:"11px"}}>objects around you.</div>
-          <div style={{marginLeft:"16px", marginTop:"11px", marginBottom:"20px"}}>The list of objects is:</div>
-          <Card text="Phone Laptop Bag Toothbrush Spoon" font={20}/>
+export default function Results3() {
+  return (
+    <React.Fragment>
+      <Link to="/Main" style={{ textDecoration: "none", margin: "5px" }}>
+        {" "}
+        &#10094;{" "}
+      </Link>
+      <div
+        className="font"
+        style={{ textAlign: "center", fontWeight: "600", padding: "20px" }}
+      >
+        Detect objects around me
+      </div>
+      <div className="font" style={{ fontSize: "20px", fontWeight: "600" }}>
+        <div style={{ marginLeft: "16px", marginBottom: "11px" }}>
+          There are:{" "}
         </div>
-      </React.Fragment>
-  
-    )
-  }
+        <Card text="4" />
+        <div style={{ marginLeft: "16px", marginTop: "11px" }}>
+          objects around you.
+        </div>
+        <div
+          style={{
+            marginLeft: "16px",
+            marginTop: "11px",
+            marginBottom: "20px",
+          }}
+        >
+          The list of objects is:
+        </div>
+        <div
+          to="/Results"
+          className="font detect poster"
+          style={{
+            textDecoration: "none",
+            maxWidth: "200px",
+            border: "none",
+            
+            justifyContent: "center",
+            margin: "auto",
+            padding: "35px 25px",
+            backgroundColor: "green",
+            borderRadius: "15px",
+            color: "black",
+            fontSize: "22px",
+            fontWeight: "600",
+            letterSpacing: "1px",
+            textAlign: "center",
+          }}
+        >
+          {" "}
+          <div> Phone </div>
+          <div> Laptop </div>
+          <div> Bag </div>
+          <div> Toothbrush </div>
+          <div> Spoon </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
